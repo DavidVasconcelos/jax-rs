d3a858100fdb04ff6dd6c040afba5d0aa5d274a5
David Vasconcelos <dsv10@hotmail.com>
2018-02-13 20:17:30 -0200

    Aula 4 - Json como retorno no model

--- loja-rest/src/main/java/br/com/alura/loja/modelo/Model.java
+++ loja-rest/src/main/java/br/com/alura/loja/modelo/Model.java
@@ -1,11 +1,16 @@
 package br.com.alura.loja.modelo;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 public class Model {
 
 	public String toXML() {
 		return new XStream().toXML(this);
+	}
+	
+	public String toJson() {
+		return new Gson().toJson(this);
 	}
 
 }
