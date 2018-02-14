1c612ce0bc18931bc1bbcb4d6ea0c7a5bb421135
David Vasconcelos <dsv10@hotmail.com>
2018-02-14 12:30:37 -0200

    Aula 10 - Filtros e logging

--- loja-rest/src/test/java/br/com/alura/loja/ClientTest.java
+++ loja-rest/src/test/java/br/com/alura/loja/ClientTest.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,81 +24,87 @@ import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
 
 public class ClientTest {
-	
+
 	private HttpServer server;
-	
+	private Client client;
+	private WebTarget target;
+
 	@Before
 	public void startaServidor() {
 		this.server = Servidor.inicializaServidor();
+		ClientConfig config = new ClientConfig();
+        config.register(new LoggingFilter());
+        this.client = ClientBuilder.newClient(config);
 	}
-	
+
 	@After
 	public void mataServidor() {
 		this.server.stop();
 	}
-	
+
 	@Test
-	public void testaConexaoComServidor () {
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
+	public void testaConexaoComServidor() {
+
+		this.target = client.target("http://www.mocky.io");
 		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
-		
+
 	}
-	
+
 	@Test
-	public void testaQueBuscaUmCarrinhoTrazEsperado () {
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
+	public void testaQueBuscaUmCarrinhoTrazEsperado() {
+
+		this.target = client.target("http://localhost:8080");
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
-		
+
 	}
-	
-	@Test
-    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/projetos/1").request().get(String.class);
-        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
 
-
-    }
-	
 	@Test
-	public void testaQueBuscaUmProjetoTrazEsperado () {
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
+	public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+
+		this.target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos/1").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+
+	}
+
+	@Test
+	public void testaQueBuscaUmProjetoTrazEsperado() {
+
+		this.target = client.target("http://localhost:8080");
 		String conteudo = target.path("/projetos/1").request().get(String.class);
 		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 		assertEquals("Minha loja", projeto.getNome());
-		
-	}
-	
-	
-	@Test
-	public void testaAdicionaCarrinhoViaPost () {
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		Carrinho carrinho = new Carrinho();
-        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
-        carrinho.setRua("Rua Vergueiro");
-        carrinho.setCidade("Sao Paulo");
-        String xml = carrinho.toXML();
-        
-        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
 
-        Response response = target.path("/carrinhos").request().post(entity);
-        Assert.assertEquals("http://localhost:8080/carrinhos/2", response.getHeaderString("Location")); 
-		
 	}
-	
-	
-	
+
+	@Test
+	public void testaAdicionaCarrinhoViaPost() {
+
+		this.target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("Sao Paulo");
+		String xml = carrinho.toXML();
+
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals("http://localhost:8080/carrinhos/2", response.getHeaderString("Location"));
+
+	}
+
+	@Test
+	public void testaDeletaProdutoCarrinhoViaDelete() {
+
+		this.target = client.target("http://localhost:8080");
+
+		Response response = target.path("/carrinhos/1/produtos/6237").request().delete();
+		Assert.assertEquals(200, response.getStatus());
+
+	}
 
 }
